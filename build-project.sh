2c95270b25d84e81c4119efa8f7b6e1ece10faba
Robin Barling <robin@silverstripe.com>
2024-04-23 16:14:00 +1200

    Implement non-root user, necessary changes to file permissions and other changes to make it compatible with current version of dash code.

---
+++ build-project.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+
+# Copy the project to another directory so that the non-root user can work with it.
+cd ~/
+mkdir -p site
+cp -rp "${APP_DIR}"/. site
+cd site/
+
+# Now build it.
+source ~/funcs.sh
+
+if [ -d ".git" ]; then
+	SHA=$(git rev-parse HEAD)
+else
+	echo "Unable to determine SHA, failing."	
+	exit 1
+fi
+
+if [[ "z${IDENT_KEY}" == "z" ]]; then
+	echo "No deploy key set"
+else
+	mkdir -p ~/.ssh
+	echo "${IDENT_KEY}" > ~/.ssh/id_rsa
+	chmod 0600 ~/.ssh/id_rsa
+	FINGER_PRINT=$(ssh-keygen -E md5 -lf ~/.ssh/id_rsa | awk '{ print $2 }' | cut -c 5-)
+	echo "Using deploy key ${FINGER_PRINT}"
+fi
+
+# Docs mention the cache is at /tmp/cache. Make sure this is true!
+export COMPOSER_HOME="/tmp"
+
+# Provide simple fingerprint for detecting that a deployment is in progress
+export CLOUD_BUILD=1
+
+# Disable vendor-expose during composer install (CMS 4+)
+if [[ -f composer.lock && "$(cat composer.lock | jq '.packages[] | select(.name == "silverstripe/vendor-plugin")')" != "" ]]; then
+	disable_postinstall_vendor_expose
+fi
+
+composer_install
+
+if [ "${CLOUD_BUILD_DISABLED}" == "" ]; then
+	echo "Running cloud-build..."
+
+	# Run NPM/Yarn build script if the cloud-build command is defined
+	if [[ -f package.json && "$(cat package.json | jq '.scripts["cloud-build"]?')" != "null" ]]; then
+		set_node_version
+
+		node_build
+	fi
+
+	# Run Composer build script if the cloud-build command is defined
+	if [[ -f composer.json && "$(cat composer.json | jq '.scripts["cloud-build"]?')" != "null" ]]; then
+		composer_build
+	fi
+
+fi
+
+# Manually run vendor-expose once scripts have run (CMS 4+)
+if [[ -f composer.lock && "$(cat composer.lock | jq '.packages[] | select(.name == "silverstripe/vendor-plugin")')" != "" ]]; then
+	composer_vendor_expose
+fi
+
+package_source ${SHA}
